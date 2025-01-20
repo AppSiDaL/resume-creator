a4797b68d3d2bf9a4bd482be001b84ab49ec74d0
AppSiDaL <gilielcrack343@gmail.com>
2025-01-19 20:53:05 -0600

    Add error handling and logging to getResumeUserInfo function

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,38 +1,37 @@
 import prisma from "./prisma";
 
 export const getResumeUserInfo = async () => {
-  const resumeInfo = await prisma.resumes.findUnique({
-    where: { id: 1 },
-    include: {
-      user: true,
-      resume_user_info: true,
-      Skills_categories: {
-        include: {
-          Skills: true,
+  try {
+    const resumeInfo = await prisma.resumes.findUnique({
+      where: { id: 1 },
+      include: {
+        user: true,
+        resume_user_info: true,
+        Skills_categories: {
+          include: {
+            Skills: true,
+          },
+        },
+        works_resumes: {
+          include: {
+            Work_info: true,
+          },
+        },
+        certifications_resumes: {
+          include: {
+            Certificacion_info: true,
+          },
+        },
+        links_networks: {
+          include: {
+            link_info: true,
+          },
         },
       },
-      works_resumes: {
-        include: {
-          Work_info: true,
-        },
-      },
-      certifications_resumes: {
-        include: {
-          Certificacion_info: true,
-        },
-      },
-      links_networks: {
-        include: {
-          link_info: true,
-        },
-      },
-    },
-  });
+    });
 
-  if (!resumeInfo) {
-    throw new Error("Resume not found");
+    console.log(JSON.stringify(resumeInfo, null, 2));
+  } catch (error) {
+    console.error(error);
   }
-  console.log(JSON.stringify(resumeInfo, null, 2));
-
-  return resumeInfo
 };
